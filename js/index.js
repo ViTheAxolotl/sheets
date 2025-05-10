98ba6ba8e968097f583d20c8524f0ed40a24c7ba
Vi <Vitheaxolotl@gmail.com>
2025-05-10 18:15:01 -0400

    ?

--- js/index.js
+++ js/index.js
@@ -85,7 +85,7 @@ function handleShowSheet()
 
     div.innerHTML = 
     `
-    <div id = "sheet" style="display: none;" draggable="false">
+    <div id = "sheet" draggable="false">
         <iframe id="statSheet" src="stats.html?${this.innerHTML}" title="stats" style="height: 90vh; margin: 5px;" draggable="false"></iframe>
     </div>
     `; 
