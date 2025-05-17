3240345f1a50bbc7faf4310421773fae39597445
Vi <Vitheaxolotl@gmail.com>
2025-05-17 18:54:55 -0400

    ?

--- js/stats.js
+++ js/stats.js
@@ -152,7 +152,7 @@ function updateStat()
 
         for(let i = 0; i < setTo.length; i++)
         {
-            if(setTo[i][0] != "•" && setTo[0] == " "){setTo[i] = "•   " + setTo[i];}
+            if(setTo[i][0] != "•" && setTo[0] != " "){setTo[i] = "•   " + setTo[i];}
         }
 
         setTo = setTo.filter(item => item !== '•   ');
